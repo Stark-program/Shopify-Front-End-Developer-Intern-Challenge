d92cac04acd1739d0767ccf5c8b10200f43ddd9a
Chad Stark <chad.d.stark@hotmail.com>
2022-05-09 16:55:54 -0700

    added axios and worked on API key

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import 'dotenv/config'
+
+
+let openAi_secret_key = process.env.OPENAI_SECRET_KEY
 
 type Data = {
   name: string
+  key: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(openAi_secret_key)
+  res.status(200).json({ name: 'John Doe', key: `${openAi_secret_key}` })
 }

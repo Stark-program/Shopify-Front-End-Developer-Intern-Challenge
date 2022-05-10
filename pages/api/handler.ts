193dded0c9d2009bc3993c7397edf48123d8e6c3
Chad Stark <chad.d.stark@hotmail.com>
2022-05-10 14:53:55 -0700

    Receive responses from the engine, got a basic form and submit button completed.

---
+++ pages/api/handler.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import 'dotenv/config'
+import axios from 'axios'
+
+
+let openAi_secret_key = process.env.OPENAI_SECRET_KEY
+
+type Data = {
+  name: string
+  key: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  console.log(req.body)
+  let config = {
+    headers: {Authorization: `Bearer ${openAi_secret_key}`}
+  }
+  let resp = await axios.post("https://api.openai.com/v1/engines/text-curie-001/completions", req.body, config)
+  console.log(resp.data.choices[0].text)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,17 +8,36 @@ import axios from 'axios'
   THE ORIGINAL PROMPT AND A RESPONSE FROM THE API.
 */
 const Home: NextPage = () => {
-  function test() {
-    let data = {
-      hello: "world"
-    }
-    axios.post("/api/hello", data).then((res) => {
-      console.log(res)
-    })
+
+  async function test(e:any) {
+    e.preventDefault()
+    let inputValue = e.target[0].value
+  let data = {
+    prompt: inputValue,
+    max_tokens: 64,
+    top_p: 1.0,
+    temperature: 0
+  }
+  try {
+    const res = await axios.post("/api/handler", data)
+    console.log(res)
+  } catch (err) {
+    console.log(err)
+  }
   }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <button onClick={test}>test</button>
+      <div className='max-w-xl'>
+        <h1 className='text-center text-[30px]'>Fun With Roofus!</h1>
+        <p className='text-center mx-2'>Below you will find an input field! In this field you are welcome to ask Roofus any questions you desire! Roofus is an AI eginnered bot that is programmed to give you the best response catered to your question. This is done utilizing GPT-3 created by <span className='text-blue-600 underline'><a href="https://openai.com/api/" target="_blank">OpenAI</a></span>!</p>
+      </div>
+      <form className="flex flex-col mt-10" onSubmit={test}>
+        <label className="flex justify-center"htmlFor='userInput'>Ask the bot a question!</label>
+        <textarea id="userInput" name="userInput" className="border-2 border-gray-700" rows={20} cols={50}></textarea>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2" type="submit">
+          Submit Question!
+        </button> 
+      </form>
     </div>
   )
 }
